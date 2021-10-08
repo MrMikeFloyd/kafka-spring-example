2194fcc9556f83036164c0270fb4e05d760f86dd
lukasmaier <lukas.maier@codecentric.de>
2021-10-08 14:44:16 +0200

    Running example to aggregate distance and calculate max speed

--- kafka-samples-streams/src/main/kotlin/com/example/kafkasamplesstreams/KafkaStreamsHandler.kt
+++ kafka-samples-streams/src/main/kotlin/com/example/kafkasamplesstreams/KafkaStreamsHandler.kt
@@ -1,5 +1,7 @@
 package com.example.kafkasamplesstreams
 
+import com.fasterxml.jackson.core.type.TypeReference
+import com.fasterxml.jackson.databind.ObjectMapper
 import org.apache.kafka.common.serialization.Serdes
 import org.apache.kafka.streams.KeyValue
 import org.apache.kafka.streams.kstream.KStream
@@ -15,31 +17,53 @@ import org.springframework.context.annotation.Configuration
 @Configuration
 class KafkaStreamsHandler {
 
+    val storeName = "telemetryDataStore"
+
     @Bean
     fun handleStream(): java.util.function.Function<KStream<String, String>, KStream<String, String>> {
         return java.util.function.Function<KStream<String, String>, KStream<String, String>> {
-            it.transform({ StateStoreTransformer() }, "exampleStore")
+            it.transform({ StateStoreTransformer() }, storeName)
         }
     }
 
     @Bean
     fun myStore(): StoreBuilder<*>? {
         return Stores.keyValueStoreBuilder(
-            Stores.persistentKeyValueStore("exampleStore"), Serdes.String(),
+            Stores.persistentKeyValueStore(storeName), Serdes.String(),
             Serdes.String())
     }
 
     inner class StateStoreTransformer : Transformer<String, String, KeyValue<String, String>> {
 
-        var exampleStore: KeyValueStore<String, String>? = null
+        private var exampleStore: KeyValueStore<String, String>? = null
 
         override fun init(context: ProcessorContext?) {
-            exampleStore = context!!.getStateStore("exampleStore") as KeyValueStore<String, String>
+            exampleStore = context!!.getStateStore(storeName) as KeyValueStore<String, String>
         }
 
         override fun transform(key: String?, value: String?): KeyValue<String, String> {
+            // Calculate travelled distance and max speed per probe
+            val typeRef: TypeReference<HashMap<String, String>> = object : TypeReference<HashMap<String, String>>() {}
+            val newTelemetryData: MutableMap<String, String> = ObjectMapper().readValue(value, typeRef)
+            val lastMeasuredSpeedMph: Double = newTelemetryData["currentSpeedMph"]!!.toDouble()
+            val feetTravelledSinceLastMeasurement = newTelemetryData["traveledDistanceFeet"]!!.toDouble()
+
             val valueFromStore = exampleStore!!.get(key)
-            val output = if (valueFromStore != null) value.plus(",").plus(valueFromStore) else value
+
+            var maxSpeedFromStore = 0.0
+            var aggregatedFeetFromStore = 0.0
+            if (valueFromStore != null) {
+                val aggregatedTelemetryData: MutableMap<String, String> =
+                    ObjectMapper().readValue(valueFromStore, typeRef)
+                maxSpeedFromStore = aggregatedTelemetryData["maxSpeed"]!!.toDouble()
+                aggregatedFeetFromStore = aggregatedTelemetryData["sumDistance"]!!.toDouble()
+            }
+
+            val sumDistance = feetTravelledSinceLastMeasurement + aggregatedFeetFromStore
+            val maxSpeed = if (lastMeasuredSpeedMph > maxSpeedFromStore) lastMeasuredSpeedMph else maxSpeedFromStore
+
+            val output = ObjectMapper().writeValueAsString(mapOf("sumDistance" to sumDistance.toString(),
+                "maxSpeed" to maxSpeed.toString()))
             exampleStore!!.put(key, output)
             return KeyValue(key, output)
         }
