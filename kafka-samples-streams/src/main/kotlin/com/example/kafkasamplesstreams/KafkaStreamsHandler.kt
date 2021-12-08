838e1fc8dd3aea2d5d1e0bd3cce1f97a6950a3d2
Maik Fleuter <maik.fleuter@codecentric.de>
2021-12-08 14:56:26 +0100

    Spike KTable implementation that avoids handling transformers and state stores manually

--- kafka-samples-streams/src/main/kotlin/com/example/kafkasamplesstreams/KafkaStreamsHandler.kt
+++ kafka-samples-streams/src/main/kotlin/com/example/kafkasamplesstreams/KafkaStreamsHandler.kt
@@ -4,19 +4,19 @@ import com.example.kafkasamplesstreams.events.AggregatedTelemetryData
 import com.example.kafkasamplesstreams.events.SpaceAgency
 import com.example.kafkasamplesstreams.events.TelemetryDataPoint
 import com.example.kafkasamplesstreams.serdes.AggregateTelemetryDataSerde
+import mu.KotlinLogging
 import org.apache.kafka.common.serialization.Serdes
 import org.apache.kafka.streams.kstream.KStream
+import org.apache.kafka.streams.kstream.Materialized
 import org.apache.kafka.streams.kstream.Predicate
-import org.apache.kafka.streams.state.KeyValueStore
-import org.apache.kafka.streams.state.StoreBuilder
-import org.apache.kafka.streams.state.Stores
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
-const val STORE_NAME = "telemetryDataStore"
 
 @Configuration
 class KafkaStreamsHandler {
+
+    private val logger = KotlinLogging.logger {}
 
     @Bean
     fun aggregateTelemetryData(): java.util.function.Function<
@@ -25,7 +25,20 @@ class KafkaStreamsHandler {
         return java.util.function.Function<
                 KStream<String, TelemetryDataPoint>,
                 Array<KStream<String, AggregatedTelemetryData>>> {
-            it.transform({ TelemetryAggregationTransformer() }, STORE_NAME)
+            it
+                .groupByKey()
+                .aggregate(
+                    { AggregatedTelemetryData("INIT", 0.0, 0.0, SpaceAgency.NASA) },
+                    { probeId, lastTelemetryReading, currentAggregatedValue ->
+                        calculateAggregateProbeData(
+                            probeId,
+                            lastTelemetryReading,
+                            currentAggregatedValue
+                        )
+                    },
+                    Materialized.with(Serdes.StringSerde(), AggregateTelemetryDataSerde())
+                )
+                .toStream()
                 .branch(
                     // Split up the processing pipeline depending on the space agency of the probe
                     Predicate { _, v -> v.spaceAgency == SpaceAgency.NASA },
@@ -34,17 +47,32 @@ class KafkaStreamsHandler {
         }
     }
 
-    @Bean
-    fun aggregatedTelemetryDataStateStore(): StoreBuilder<KeyValueStore<String, AggregatedTelemetryData>> {
-        return Stores.keyValueStoreBuilder(
-            /**
-             * Choosing an inMemory Store ensures that the kafka streams application
-             * doesn't store any data on disk (via RocksDb) and our state is clear
-             * when we delete the changelog topic and restart our application
-             **/
-            Stores.inMemoryKeyValueStore(STORE_NAME),
-            Serdes.String(),
-            AggregateTelemetryDataSerde()
+    /**
+     * Performs calculation of per-probe aggregate measurement data.
+     * The currently calculated totals are held in a Kafka State Store
+     * backing the KTable created with aggregate() and the most recently
+     * created aggregate telemetry data record is passed on downstream.
+     */
+    fun calculateAggregateProbeData(
+        probeId: String,
+        lastTelemetryReading: TelemetryDataPoint,
+        currentAggregatedValue: AggregatedTelemetryData
+    ): AggregatedTelemetryData {
+        val totalDistanceTraveled =
+            lastTelemetryReading.traveledDistanceFeet + currentAggregatedValue.traveledDistanceFeet
+        val maxSpeed = if (lastTelemetryReading.currentSpeedMph > currentAggregatedValue.maxSpeedMph)
+            lastTelemetryReading.currentSpeedMph else currentAggregatedValue.maxSpeedMph
+        val aggregatedTelemetryData = AggregatedTelemetryData(
+            probeId = lastTelemetryReading.probeId,
+            traveledDistanceFeet = totalDistanceTraveled,
+            maxSpeedMph = maxSpeed,
+            spaceAgency = lastTelemetryReading.spaceAgency
         )
+        logger.info {
+            "Calculated new aggregated telemetry data for probe $probeId. New max speed: ${aggregatedTelemetryData.maxSpeedMph} and " +
+                    "travelled distance ${aggregatedTelemetryData.traveledDistanceFeet}"
+
+        }
+        return aggregatedTelemetryData
     }
 }
